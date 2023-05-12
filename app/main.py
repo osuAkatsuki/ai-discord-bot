e06d8cf3b8cfe1aae66aead585dc1b9af747e822
cmyui <cmyuiosu@gmail.com>
2023-05-11 21:18:12 -0400

    rewrite some ways the bot works

--- app/main.py
+++ app/main.py
@@ -110,15 +110,15 @@ async def on_message(message: discord.Message):
     if bot.user is None:
         return
 
-    # they are in threads
-    if not isinstance(message.channel, discord.Thread):
-        return
-
-    # they are not from us (the bot)
+    # they are not from us
     if message.author.id == bot.user.id:
         return
 
-    # they are in a thread that was created with /askai
+    # we are mentioned in the message
+    if bot.user not in message.mentions:
+        return
+
+    # they are in a thread that we are tracking
     tracked_thread = await threads.fetch_one(message.channel.id)
     if tracked_thread is None:
         return
@@ -126,18 +126,6 @@ async def on_message(message: discord.Message):
     prompt = message.content
     if prompt.startswith(f"{bot.user.mention} "):
         prompt = prompt.removeprefix(f"{bot.user.mention} ")
-
-    if prompt.startswith("! "):
-        prompt = prompt.removeprefix("! ")
-        send_to_gpt = True
-        messages_of_context = 20
-    elif prompt.startswith("!! "):
-        prompt = prompt.removeprefix("!! ")
-        send_to_gpt = True
-        messages_of_context = 50
-    else:
-        send_to_gpt = False
-        messages_of_context = 0
 
     prompt = f"{message.author.display_name}: {prompt}"
 
@@ -148,39 +136,34 @@ async def on_message(message: discord.Message):
         tokens_used=0,
     )
 
-    # if it started with a "! " or "!! ", ask gpt for a response
-    if send_to_gpt:
-        thread_history = await thread_messages.fetch_many(thread_id=message.channel.id)
+    thread_history = await thread_messages.fetch_many(thread_id=message.channel.id)
 
-        message_history = []
-        if messages_of_context:
-            message_history.extend(
-                {"role": m["role"], "content": m["content"]}
-                for m in thread_history[-messages_of_context:]
-            )
-        message_history.append({"role": "user", "content": prompt})
+    message_history = [
+        {"role": m["role"], "content": m["content"]}
+        # keep 10 messages before the prompt
+        # TODO: allow some users to configure this per-thread
+        for m in thread_history[-10:]
+    ]
+    message_history.append({"role": "user", "content": prompt})
 
-        gpt_response = await openai.ChatCompletion.acreate(
-            model=tracked_thread["model"],
-            messages=message_history,
-        )
-        assert isinstance(gpt_response, OpenAIObject)  # TODO: can we do better?
+    gpt_response = await openai.ChatCompletion.acreate(
+        model=tracked_thread["model"],
+        messages=message_history,
+    )
+    assert isinstance(gpt_response, OpenAIObject)  # TODO: can we do better?
 
-        gpt_response_content = gpt_response.choices[0].message.content
-        tokens_spent = gpt_response.usage.total_tokens
-        dollars_spent = openai_pricing.tokens_to_dollars(
-            tracked_thread["model"], tokens_spent
-        )
+    gpt_response_content = gpt_response.choices[0].message.content
+    tokens_spent = gpt_response.usage.total_tokens
 
-        for chunk in split_message(gpt_response_content, 2000):
-            await message.channel.send(chunk)
+    for chunk in split_message(gpt_response_content, 2000):
+        await message.channel.send(chunk)
 
-        await thread_messages.create(
-            message.channel.id,
-            gpt_response_content,
-            role="assistant",
-            tokens_used=tokens_spent,
-        )
+    await thread_messages.create(
+        message.channel.id,
+        gpt_response_content,
+        role="assistant",
+        tokens_used=tokens_spent,
+    )
 
 
 @command_tree.command(name="cost")
@@ -193,22 +176,22 @@ async def cost(interaction: discord.Interaction):
         return
 
     messages = await thread_messages.fetch_many(thread_id=interaction.channel.id)
+    users = []
     tokens_used = sum(m["tokens_used"] for m in messages)
     response_cost = openai_pricing.tokens_to_dollars(thread["model"], tokens_used)
 
     await interaction.response.send_message(
-        f"This thread has used ${response_cost:.5f} ({tokens_used} tokens) over {len(messages)} messages",
+        f"The running total of this thread is ${response_cost:.5f} ({tokens_used} tokens) over {len(messages)} messages",
         ephemeral=True,
     )
 
 
-@command_tree.command(name="askai")
-async def ask_ai(
+@command_tree.command(name="ai")
+async def ai(
     interaction: discord.Interaction,
-    initial_prompt: str,
     model: Literal["gpt-4", "gpt-3.5-turbo"] = "gpt-4",
 ):
-    if interaction.user.id not in allowed_to_ask_ai:
+    if interaction.user.id not in allowed_to_prompt_ai:
         await interaction.response.send_message(
             "You are not allowed to use this command",
             ephemeral=True,
@@ -220,20 +203,12 @@ async def ask_ai(
     assert interaction.channel is not None
     assert isinstance(interaction.channel, discord.TextChannel)
 
-    gpt_response = await openai.ChatCompletion.acreate(
-        model=model,
-        messages=[{"role": "user", "content": initial_prompt}],
-    )
-    assert isinstance(gpt_response, OpenAIObject)  # TODO: can we do better?
-
-    tokens_used = gpt_response.usage.total_tokens
-    response_cost = openai_pricing.tokens_to_dollars(model, tokens_used)
-
     thread_creation_message = await interaction.followup.send(
-        content=(
-            f"Created thread for ${response_cost:.5f} ({tokens_used} tokens)\n"
-            "\n"
-            f"Your prompt: **{initial_prompt}**\n"
+        content="\n".join(
+            (
+                f"**Opening a thread with {model}**",
+                f"Model Rate: ${openai_pricing.price_for_model(model)}/1000 tokens",
+            )
         ),
         ephemeral=True,
         wait=True,
@@ -241,30 +216,12 @@ async def ask_ai(
 
     thread_creation_message.guild = interaction.guild  # needed for thread. weird.
     thread = await thread_creation_message.create_thread(
-        name=f"AI Thread: {initial_prompt[:30] + ('...' if len(initial_prompt) > 30 else '')}",
+        name=f"@{interaction.user.display_name}'s AI Thread",
     )
     await threads.create(
         thread.id,
         initiator_user_id=interaction.user.id,
         model=model,
-    )
-
-    gpt_response_content = gpt_response.choices[0].message.content
-
-    for chunk in split_message(gpt_response_content, 2000):
-        await thread.send(chunk)
-
-    await thread_messages.create(
-        thread.id,
-        content=initial_prompt,
-        role="user",
-        tokens_used=0,
-    )
-    await thread_messages.create(
-        thread.id,
-        content=gpt_response_content,
-        role="assistant",
-        tokens_used=tokens_used,
     )
 
 
