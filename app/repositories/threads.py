230de92f9d4a9333cac2a92adf9def3a9e388eec
cmyui <cmyuiosu@gmail.com>
2023-05-11 21:44:45 -0400

    add ctx length and partial update to threads

--- app/repositories/threads.py
+++ app/repositories/threads.py
@@ -1,5 +1,7 @@
 from datetime import datetime
-from typing import TypedDict, cast, Any
+from typing import Any
+from typing import cast
+from typing import TypedDict
 
 from app import state
 
@@ -7,6 +9,7 @@ READ_PARAMS = """\
     thread_id,
     initiator_user_id,
     model,
+    context_length,
     created_at
 """
 
@@ -22,16 +25,18 @@ async def create(
     thread_id: int,
     initiator_user_id: int,
     model: str,
+    context_length: int,
 ) -> Thread:
     query = f"""\
-        INSERT INTO threads (thread_id, initiator_user_id, model)
-        VALUES (:thread_id, :initiator_user_id, :model)
+        INSERT INTO threads (thread_id, initiator_user_id, model, context_length)
+        VALUES (:thread_id, :initiator_user_id, :model, :context_length)
         RETURNING {READ_PARAMS}
     """
     values: dict[str, Any] = {
         "thread_id": thread_id,
         "initiator_user_id": initiator_user_id,
         "model": model,
+        "context_length": context_length,
     }
     rec = await state.write_database.fetch_one(query, values)
     return cast(Thread, rec)
@@ -51,6 +56,7 @@ async def fetch_one(thread_id: int) -> Thread:
 async def fetch_many(
     initiator_user_id: int | None = None,
     model: str | None = None,
+    context_length: int | None = None,
     page: int | None = None,
     page_size: int | None = None,
 ) -> list[Thread]:
@@ -59,10 +65,12 @@ async def fetch_many(
         FROM threads
         WHERE initiator_user_id = COALESCE(:initiator_user_id, initiator_user_id)
         AND model = COALESCE(:model, model)
+        AND context_length = COALESCE(:context_length, context_length)
     """
     values: dict[str, Any] = {
         "initiator_user_id": initiator_user_id,
         "model": model,
+        "context_length": context_length,
     }
     if page is not None and page_size is not None:
         query += "LIMIT :page_size OFFSET :offset"
@@ -70,3 +78,27 @@ async def fetch_many(
         values["offset"] = (page - 1) * page_size
     recs = await state.read_database.fetch_all(query, values)
     return cast(list[Thread], recs)
+
+
+async def partial_update(
+    thread_id: int,
+    initiator_user_id: int | None = None,
+    model: str | None = None,
+    context_length: int | None = None,
+) -> Thread | None:
+    query = f"""\
+        UPDATE threads
+        SET initiator_user_id = COALESCE(:initiator_user_id, initiator_user_id),
+            model = COALESCE(:model, model),
+            context_length = COALESCE(:context_length, context_length)
+        WHERE thread_id = :thread_id
+        RETURNING {READ_PARAMS}
+    """
+    values: dict[str, Any] = {
+        "thread_id": thread_id,
+        "initiator_user_id": initiator_user_id,
+        "model": model,
+        "context_length": context_length,
+    }
+    rec = await state.write_database.fetch_one(query, values)
+    return cast(Thread, rec) if rec is not None else None
