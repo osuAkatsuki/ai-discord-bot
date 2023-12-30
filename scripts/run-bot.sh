6dcc2c33bd570b8cfdf4e449b5d6499a58d8555c
Josh Smith <joshua.smith@super.com>
2023-12-30 04:56:01 -0500

    base k8s deploy setup

--- scripts/run-bot.sh
+++ scripts/run-bot.sh
@@ -10,10 +10,15 @@ if [ -z "$APP_ENV" ]; then
   exit 1
 fi
 
-
 if [ -z "$APP_COMPONENT" ]; then
   echo "Please set APP_COMPONENT"
   exit 1
+fi
+
+if [[ $PULL_SECRETS_FROM_VAULT -eq 1 ]]; then
+  # TODO: revert to $APP_ENV
+  akatsuki vault get ai-discord-bot production-k8s -o .env
+  source .env
 fi
 
 cd /srv/root
