f5fb137f24313e84e386fd675ae8f60b1bcc2cc3
cmyui <cmyuiosu@gmail.com>
2023-12-31 19:47:04 -0500

    remove secrets source

--- scripts/run-bot.sh
+++ scripts/run-bot.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
-if [ -n "$KUBERNETES" ]; then
-    source /vault/secrets/secrets.txt
-fi
-
 if [ -z "$APP_ENV" ]; then
   echo "Please set APP_ENV"
   exit 1
