a68cfa6634ac37776c80aeb97ce0b906b4d480a5
cmyui <cmyuiosu@gmail.com>
2024-03-17 05:00:45 -0400

    introduce stronger typing for openai models

--- app/openai_pricing.py
+++ app/openai_pricing.py
@@ -1,55 +1,70 @@
 # https://openai.com/pricing
+from app.adapters.openai.gpt import OpenAIModel
 
 
-def input_price_per_million_tokens(model: str) -> float:
+def input_price_per_million_tokens(model: OpenAIModel) -> float:
     match model:
-        case "gpt-4-0125-preview" | "gpt-4-1106-preview" | "gpt-4-1106-vision-preview":
+        case (
+            OpenAIModel.GPT_4_TURBO_PREVIEW
+            | OpenAIModel.GPT_4_0125_PREVIEW
+            | OpenAIModel.GPT_4_1106_PREVIEW
+            | OpenAIModel.GPT_4_1106_VISION_PREVIEW
+        ):
             return 10.00
-        case "gpt-4":
+        case OpenAIModel.GPT_4:
             return 30.00
-        case "gpt-4-32k":
+        case OpenAIModel.GPT_4_32K:
             return 60.00
-        case "gpt-3.5-turbo-0125":
+        case OpenAIModel.GPT_3_5_TURBO | OpenAIModel.GPT_3_5_TURBO_0125:
             return 0.50
-        case "gpt-3.5-turbo-instruct":
+        case OpenAIModel.GPT_3_5_TURBO_INSTRUCT:
             return 1.50
-        case "gpt-3.5-turbo-1106":
+        case OpenAIModel.GPT_3_5_TURBO_1106:
             return 1.00
-        case "gpt-3.5-turbo-0613":
+        case OpenAIModel.GPT_3_5_TURBO_0613:
             return 1.50
-        case "gpt-3.5-turbo-16k-0613":
+        case OpenAIModel.GPT_3_5_TURBO_16K_0613:
             return 3.00
-        case "gpt-3.5-turbo-0301":
+        case OpenAIModel.GPT_3_5_TURBO_0301:
             return 1.50
         case _:
             raise NotImplementedError(f"Unknown model: {model}")
 
 
-def output_price_per_million_tokens(model: str) -> float:
+def output_price_per_million_tokens(model: OpenAIModel) -> float:
     match model:
-        case "gpt-4-0125-preview" | "gpt-4-1106-preview" | "gpt-4-1106-vision-preview":
+        case (
+            OpenAIModel.GPT_4_TURBO_PREVIEW
+            | OpenAIModel.GPT_4_0125_PREVIEW
+            | OpenAIModel.GPT_4_1106_PREVIEW
+            | OpenAIModel.GPT_4_1106_VISION_PREVIEW
+        ):
             return 30.00
-        case "gpt-4":
+        case OpenAIModel.GPT_4:
             return 60.00
-        case "gpt-4-32k":
+        case OpenAIModel.GPT_4_32K:
             return 120.00
-        case "gpt-3.5-turbo-0125":
+        case OpenAIModel.GPT_3_5_TURBO | OpenAIModel.GPT_3_5_TURBO_0125:
             return 1.50
-        case "gpt-3.5-turbo-instruct":
+        case OpenAIModel.GPT_3_5_TURBO_INSTRUCT:
             return 2.00
-        case "gpt-3.5-turbo-1106":
+        case OpenAIModel.GPT_3_5_TURBO_1106:
             return 2.00
-        case "gpt-3.5-turbo-0613":
+        case OpenAIModel.GPT_3_5_TURBO_0613:
             return 2.00
-        case "gpt-3.5-turbo-16k-0613":
+        case OpenAIModel.GPT_3_5_TURBO_16K_0613:
             return 4.00
-        case "gpt-3.5-turbo-0301":
+        case OpenAIModel.GPT_3_5_TURBO_0301:
             return 2.00
         case _:
             raise NotImplementedError(f"Unknown model: {model}")
 
 
-def tokens_to_dollars(model: str, input_tokens: int, output_tokens: int) -> float:
+def tokens_to_dollars(
+    model: OpenAIModel,
+    input_tokens: int,
+    output_tokens: int,
+) -> float:
     per_million_input_price = input_price_per_million_tokens(model)
     per_million_output_price = output_price_per_million_tokens(model)
     return (
