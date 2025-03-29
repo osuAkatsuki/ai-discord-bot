d53fb146f0cb31df40ec8e040cd48baf835bdf7f
RealistikDash <realistikdash@gmail.com>
2025-03-29 09:01:21 +0000

    add: enable commands in private channels and dms (#34)

--- app/main.py
+++ app/main.py
@@ -33,8 +33,19 @@ MAX_CONTENT_LENGTH = 100
 intents = discord.Intents.default()
 intents.message_content = True
 
+allowed_installs = discord.app_commands.AppInstallationType(guild=True, user=True)
+allowed_contexts = discord.app_commands.AppCommandContext(
+    guild=True,
+    dm_channel=True,
+    private_channel=True,
+)
+
 bot = DiscordBot(intents=intents)
-command_tree = discord.app_commands.CommandTree(bot)
+command_tree = discord.app_commands.CommandTree(
+    bot,
+    allowed_contexts=allowed_contexts,
+    allowed_installs=allowed_installs,
+)
 
 
 def command_name(command_name: str) -> str:
